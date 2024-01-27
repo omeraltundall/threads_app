8dd37f1b8bf8294c7acb7db11c50d991ef26fcd3
Omer Altundal <altundaomer01.99@gmail.com>
2024-01-27 14:43:38 +0300

    First Commit

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+import "../globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Threads",
+  description: "A Next.js 14 Meta Threads Application",
+  icons: {
+    icon:'../favicon.ico'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body className={`${inter.className} bg-dark-1`}>
+          <div className="w-full flex justify-center items-center min-h-screen">
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,30 @@
+const { hostname } = require('os')
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+   experimental: {
+      serverComponentsExternalPackages: ["mongoose"]
+   },
+   images: {
+      remotePatterns: [
+         {
+            protocol: 'https',
+            hostname: 'img.clerk.com'
+         },
+         {
+            protocol: 'https',
+            hostname: 'img.clerk.dev'
+         },
+         {
+            protocol: 'https',
+            hostname: 'uploadthing.com'
+         },
+         {
+            protocol: 'https',
+            hostname: 'placehold.co'
+         }
+      ]
+   }
+}
 
 module.exports = nextConfig
