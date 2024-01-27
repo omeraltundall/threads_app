e22f2b0bb2fd6a2dd47a62408ec1b35b85dc5bb9
Omer Altundal <altundaomer01.99@gmail.com>
2024-01-27 16:59:08 +0300

    Minor Changes in Activity Page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -17,7 +17,7 @@ async function Page() {
 
   return (
     <>
-      <h1 className='head-text'>Activity</h1>
+      <h1 className='head-text'>Activities</h1>
 
       <section className='mt-10 flex flex-col gap-5'>
         {activity.length > 0 ? (
